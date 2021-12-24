510d01c427032f00528448f31651af3038d3d900
Balint <privatebalint@gmail.com>
2021-12-24 18:16:30 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run(); //never returns
+}
+
+struct Server {
+    address: String,
+}
+
+impl Server {
+    fn new(address: String) -> Self {
+        Server {
+            address
+        }
+    }
+
+    fn run(self) { // takes ownership, deallocates Server when terminated
+        println!("Listening on {}:", self.address);
+    }
+}
+
+
+/* 
+ * request
+ * GET /user?id=10 HTTP/1.1\r\n
+ * HEADERS \r\n
+ * BODY
+ */
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: Method
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH
 }
