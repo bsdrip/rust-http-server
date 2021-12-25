eba2308e9ace51d58fa9d0ebdf2d120d90a7a4a0
Balint <privatebalint@gmail.com>
2021-12-25 21:49:47 +0100

    fixup! WIP

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
 // server module
 use std::net::TcpListener;
+use std::io::Read;
 
 pub struct Server {
     address: String
@@ -16,5 +17,15 @@ impl Server {
         println!("Listening on {}", self.address);
 
         let listener = TcpListener::bind(&self.address).unwrap(); // ::bind returns Result<T, E> which can be (Ok(T), Err(E))
+
+        loop {
+            match listener.accept() { // hangs until new connection arrives, returns a Result<(TcpStream, SocketAddr)> because might fail
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    stream.read(&mut buffer); // reads bytes from socket
+                },
+                Err(e) => println!("Failed to establish a connection: {}", e),
+            }
+        }
     }
 }
